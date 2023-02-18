f4fa73e3dad0f681b788fd8d06561d8dfaadf6ec
najima1 <mohammednazim3629@gmail.com>
2023-02-18 05:13:24 -0600

    all done

--- api_controlar.js
+++ api_controlar.js
@@ -7,12 +7,32 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const data_base = client.db("JOBS")
 const fresher_data = data_base.collection('fresher')
 const all_data = data_base.collection("company_details")
+const topCompany = data_base.collection('top_companys')
 
 // Get all fresher jobs data
 const fresher_job = async (req, res) => {
     try {
         const query = {}
         const fresher = await fresher_data.find(query).toArray()
+
+        res.send({
+            status: true,
+            message: "Product fonud from the database",
+            data: fresher
+        })
+
+    } catch (error) {
+        return res.send({
+            status: 404,
+            message: error.message
+        })
+    }
+}
+// Get all top company jobs data
+const top_comapyFn = async (req, res) => {
+    try {
+        const query = {}
+        const fresher = await topCompany.find(query).toArray()
 
         res.send({
             status: true,
@@ -52,5 +72,6 @@ const filter_data = async (req, res) => {
 
 module.exports = {
     fresher_job,
-    filter_data
+    filter_data,
+    top_comapyFn
 }
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const cors = require('cors')
-const { fresher_job, filter_data } = require('./api_controlar')
+const { fresher_job, filter_data, top_comapyFn } = require('./api_controlar')
 const port = process.env.PORT || 8000
 
 app.use(cors())
@@ -13,6 +13,9 @@ const api_Fn = async () => {
     try {
         // get all fresher data
         app.get('/fresher', fresher_job)
+
+        //get top allcompanys
+        app.get('/top_comapy', top_comapyFn)
 
         //get single data from all details data
         app.get("/fresher/:name", filter_data)
